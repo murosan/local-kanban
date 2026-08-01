053b5520f8286d9912e19b667ceb53f8b1eaa4df
murosan <18212747+murosan@users.noreply.github.com>
2026-08-01 22:09:13 +0900

    ignore dist files on linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist', '**/dist/**', '../pkg/ui/dist/**', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
